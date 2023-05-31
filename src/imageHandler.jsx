257bfbb1b9351e1b07f8d2a4b0d77accf9943a13
jackcdavey <jdavey@scu.edu>
2023-05-31 12:27:18 -0700

    Image handling updates

--- src/imageHandler.jsx
+++ src/imageHandler.jsx
@@ -1,61 +1,33 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import axios from 'axios';
+import styles from './dropzone.module.css';
 
 const API_BASE_URL = 'https://48f7-2600-1700-5454-1640-6915-7909-b4ee-bfa4.ngrok-free.app';
 
-const ImageHandler = () => {
-    const onDrop = useCallback(async (acceptedFiles) => {
-        try {
-            const file = acceptedFiles[0];
-            const formData = new FormData();
-            formData.append('image', file);
+const ImageHandler = ({ onFileDrop, onSubmit }) => {
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [file, setFile] = useState(null);
 
-            // Send request to the generate-prompt endpoint
-            const responsePrompt = await axios.post(`${API_BASE_URL}/generate-prompt`, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-
-            // Send request to the Google Vision API endpoint
-            const responseGoogleVision = await axios.post(`${API_BASE_URL}/api/google-vision`, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-
-            // Send request to the Stable Diffusion WebUI endpoint
-            const responseStableDiffusion = await axios.post(`${API_BASE_URL}/api/stable-diffusion`, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-
-            // Send request to the color-info endpoint
-            const responseColorInfo = await axios.post(`${API_BASE_URL}/api/color-info`, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-
-            // Use the responses
-            console.log(responsePrompt.data.result);
-            console.log(responseGoogleVision.data.result);
-            console.log(responseStableDiffusion.data.result);
-            console.log(responseColorInfo.data);
-
-        } catch (error) {
-            console.error(error);
-        }
-    }, []);
+    const onDrop = useCallback((acceptedFiles) => {
+        const droppedFile = acceptedFiles[0];
+        setSelectedImage(URL.createObjectURL(droppedFile)); // Preview the image
+        setFile(droppedFile); // Save the file for later
+        onFileDrop(droppedFile);
+    }, [onFileDrop]);
 
     const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
     return (
-        <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            <p>Drag and drop some files here, or click to select files</p>
+        <div>
+            <div {...getRootProps()} className={styles.dropzone}>
+                <input {...getInputProps()} />
+                {selectedImage ? (
+                    <img src={selectedImage} alt="Preview" className={styles.previewImage} />
+                ) : (
+                    <p>Drag and drop an image here, or click to select an image</p>
+                )}
+            </div>
         </div>
     );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Dropzone from '../imageHandler';
@@ -6,11 +5,55 @@ import Navbar from './components/navbar';
 
 export default function Home() {
   const router = useRouter();
+  const [file, setFile] = useState(null);
   const [userInput, setUserInput] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // TODO: handle image upload
+const handleSubmit = async () => {
+        if (!file) return;
+
+        try {
+            const formData = new FormData();
+            formData.append('image', file);
+
+            // Send request to the generate-prompt endpoint
+            const responsePrompt = await axios.post(`${API_BASE_URL}/generate-prompt`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+
+            // Send request to the Google Vision API endpoint
+            const responseGoogleVision = await axios.post(`${API_BASE_URL}/api/google-vision`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+
+            // Send request to the Stable Diffusion WebUI endpoint
+            const responseStableDiffusion = await axios.post(`${API_BASE_URL}/api/stable-diffusion`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+
+            // Send request to the color-info endpoint
+            const responseColorInfo = await axios.post(`${API_BASE_URL}/api/color-info`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+
+            // Use the responses
+            console.log(responsePrompt.data.result);
+            console.log(responseGoogleVision.data.result);
+            console.log(responseStableDiffusion.data.result);
+            console.log(responseColorInfo.data);
+
+        } catch (error) {
+            console.error(error);
+        }
+    console.log(file); // You should see the selected file logged in the console
+
     router.push('/highlight');
   };
 
@@ -18,7 +61,7 @@ export default function Home() {
     <div>
       <Navbar />
       <h1>Your AI Plant Arranger</h1>
-      <Dropzone />
+      <Dropzone onFileDrop={setFile} />
       <form onSubmit={handleSubmit}>
         <input 
           type="text" 
